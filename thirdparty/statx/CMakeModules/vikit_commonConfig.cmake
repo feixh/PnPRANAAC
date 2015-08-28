10b9ebca7b9384bd51b410a478415e92780d76f2
feixh <hzhsfxh@gmail.com>
2015-08-28 16:27:51 -0700

    hmm, build optimo and statx successfully

---
+++ thirdparty/statx/CMakeModules/vikit_commonConfig.cmake
@@ -0,0 +1,17 @@
+#######################################################
+# vikit_common source dir
+set( vikit_common_SOURCE_DIR "/home/feixh/ClionProjects/xfv_svo/thirdparty/rpg_vikit/vikit_common")
+
+#######################################################
+# vikit_common build dir
+set( vikit_common_DIR "/home/feixh/ClionProjects/xfv_svo/thirdparty/rpg_vikit/vikit_common/build")
+
+#######################################################
+set( vikit_common_INCLUDE_DIR  "/home/feixh/ClionProjects/xfv_svo/thirdparty/rpg_vikit/vikit_common/include" )
+set( vikit_common_INCLUDE_DIRS "/home/feixh/ClionProjects/xfv_svo/thirdparty/rpg_vikit/vikit_common/include" )
+
+set( vikit_common_LIBRARIES    "/home/feixh/ClionProjects/xfv_svo/thirdparty/rpg_vikit/vikit_common/lib/libvikit_common.so" )
+set( vikit_common_LIBRARY      "/home/feixh/ClionProjects/xfv_svo/thirdparty/rpg_vikit/vikit_common/lib/libvikit_common.so" )
+
+set( vikit_common_LIBRARY_DIR  "/home/feixh/ClionProjects/xfv_svo/thirdparty/rpg_vikit/vikit_common/build" )
+set( vikit_common_LIBRARY_DIRS "/home/feixh/ClionProjects/xfv_svo/thirdparty/rpg_vikit/vikit_common/build" )
