42e8ddf403ed50346a5666d89e11696ca12820a3
feixh <hzhsfxh@gmail.com>
2016-03-11 14:46:24 -0800

    fix header

--- include/ransac_estimators.h
+++ include/ransac_estimators.h
@@ -24,10 +24,6 @@
 
 namespace ransac_estimators
 {
-	using namespace theia;
-	using namespace Eigen;
-	using namespace std;
-	
 struct Match2D3D
 {
     Eigen::Vector3d featureVector;  // unitary bearing vectors
@@ -35,10 +31,10 @@ struct Match2D3D
 };
 
 
-class P3PEstimator : public Estimator< Match2D3D, Matrix<double, 3, 4 > > {
+class P3PEstimator : public theia::Estimator< Match2D3D, Eigen::Matrix<double, 3, 4 > > {
 public:
     P3PEstimator():
-        Estimator< Match2D3D, Matrix<double, 3, 4> >(),
+        Estimator< Match2D3D, Eigen::Matrix<double, 3, 4> >(),
         solver(){}
 
 // Get the minimum number of samples needed to generate a model.
@@ -52,8 +48,8 @@ public:
     // estimation. Typically, this is a minimal set, but it is not required to be.
     virtual bool EstimateModel(const std::vector<Datum> &data, std::vector<Model> *model) const {
         assert(data.size() >= 3);
-        Matrix3d featureVectors;
-        Matrix3d worldPoints;
+        Eigen::Matrix3d featureVectors;
+        Eigen::Matrix3d worldPoints;
         for (size_t i = 0; i < 3; ++i) {
             featureVectors.col(i) = data[i].featureVector;
             worldPoints.col(i)    = data[i].worldPoint;
@@ -79,12 +75,12 @@ public:
   // this function appropriately for the task being solved.
   virtual double Error(const Datum& data, const Model& model) const {
       // model is gwc
-      const Vector3d &worldPoint( data.worldPoint );
-      Vector3d proj( model.block<3,3>(0,0).transpose()*( worldPoint - model.block<3,1>(0,3) ) );
+      const Eigen::Vector3d &worldPoint( data.worldPoint );
+      Eigen::Vector3d proj( model.block<3,3>(0,0).transpose()*( worldPoint - model.block<3,1>(0,3) ) );
       if ( proj(2) < 0 ){
           return 1000000;
       }
-      const Vector3d &featureVector( data.featureVector );
+      const Eigen::Vector3d &featureVector( data.featureVector );
       double dx( featureVector(0)/featureVector(2) - proj(0)/proj(2) );
       double dy( featureVector(1)/featureVector(2) - proj(1)/proj(2) );
       return dx*dx + dy*dy;
